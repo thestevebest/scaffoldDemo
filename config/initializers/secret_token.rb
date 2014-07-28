5e1ef1ed2c66375e51e3f84f84f934976260c400
Steve Best <steve@app-factory.com>
2014-07-28 11:11:07 -0700

    Initializing repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScaffoldDemo::Application.config.secret_key_base = '38a00834155a18516cb6fe41fa1e43d5d554e0f69f4a836af34c2e3e2b29e23f1dd32b8ec2c2f1c0166f0dd2a13754753f0d93223f734058cb4ffacaddce3927'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScaffoldDemo::Application.config.session_store :cookie_store, key: '_scaffoldDemo_session'
